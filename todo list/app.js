a0121de455c3afd33fb40d7261f92321efa41126
AyxanAmirov <amirovayxan0@gmail.com>
2024-04-16 03:51:10 -0700

    Todo List End

--- todo list/app.js
+++ todo list/app.js
@@ -2,7 +2,27 @@ const addTodoForm = document.querySelector("#add-body");
 const addInput = document.querySelector("#add-input");
 const todoList = document.querySelector(".list-body");
 const removeAllTodos = document.querySelector("#remove-all-Todos");
+const searchInput = document.querySelector("#search-input");
 let myTodos = [];
+
+searchInput.addEventListener("keyup", searchTodo);
+
+function searchTodo() {
+  let searchValue = searchInput.value.toLowerCase()
+  let listElements = document.querySelectorAll(".list-elem");
+  if (listElements.length > 0) {
+    listElements.forEach(todo =>{
+      if (todo.textContent.toLowerCase().trim().includes(searchValue)) {
+        todo.setAttribute("style","display : block");
+      } else {
+        todo.setAttribute("style","display : none")
+      }                             
+    });
+  }else{
+    alert("Ən az 1 todo daxil edin")
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   const myTodo = localStorage.getItem("todo");
   if (myTodo) {
@@ -16,8 +36,8 @@ addTodoForm.addEventListener("submit", addTodo);
 
 removeAllTodos.addEventListener("click", () => {
   todoList.innerHTML = "";
-  myTodos = []; 
-  localStorage.removeItem("todo"); 
+  myTodos = [];
+  localStorage.removeItem("todo");
 });
 
 function addTodo(e) {
@@ -32,7 +52,6 @@ function addTodo(e) {
     alert("Dəyər daxil edin");
   }
   addInput.value = "";
-
 }
 
 function deleteTodo(todoElement) {
